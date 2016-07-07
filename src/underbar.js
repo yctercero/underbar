17c39de70c59c4eba6e3c07e873621c58878b997
Yara TP <ytercero36@gmail.com>
2016-07-07 18:04:31 -0400

    Got memoize working. May be a long way with some extra steps but is functioning.

--- src/underbar.js
+++ src/underbar.js
@@ -364,20 +364,27 @@
     var memo = {};
 
     return function(){
-      var args = arguments;
-        // returns array of the arguments
+      //var args = Array.prototype.slice.call(arguments);
+      //var args = "'" + Array.prototype.slice.call(arguments) + "'";
+        // [1, 2, 3] gets stored as '1,2,3'
+      // var args = arguments;
+        // gets stored in memo as 'object Arguments'
+      var argToAdd = {};
+      
+      _.each(arguments, function(item, key){
+          
+          argToAdd[key] = item;
+      });
 
-      console.log(args);
-      console.log(memo[args]);
+      //http://stackoverflow.com/questions/5612787/converting-an-object-to-a-string
+      var args = JSON.stringify(argToAdd);
+
       if(args in memo){
-        console.log(memo[args]);
-        console.log(memo);
         return memo[args]; //would return value a.k.a. 'result'
       } else{
         var result;
         result = func.apply(this, arguments);
         memo[args] = result;
-        console.log(memo);
         return result;
       }
     }
