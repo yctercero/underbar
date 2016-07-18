6ce97aa2f4c43a7084422e50027dd3bee6e5ac98
Yara TP <ytercero36@gmail.com>
2016-07-18 17:31:16 -0400

    Fixed reject.

--- src/underbar.js
+++ src/underbar.js
@@ -101,7 +101,7 @@
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
 
-    return filter(collection, function(item){
+    return _.filter(collection, function(item){
       console.log(test(item));
       return !test(item);
     });
